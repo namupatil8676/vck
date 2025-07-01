a1badc413b724e0d3c74a56ac1361c8b7ab89017
18aman2 <amanshaikh2020as@gmail.com>
2025-07-01 15:03:27 +0530

    fifth commit

--- src/components/Developerinfo/Developerinfopopup.jsx
+++ src/components/Developerinfo/Developerinfopopup.jsx
@@ -1,5 +1,5 @@
 // Example structure for a React component (conceptual)
-import './DeveloperInfoPopup.css'
+import './Devloperinfopopup.css'
 function DeveloperInfoPopup({ show, onClose, studentName, studentPhotoUrl, uniqueMessage }) {
   if (!show) {
     return null; // Don't render if not visible
